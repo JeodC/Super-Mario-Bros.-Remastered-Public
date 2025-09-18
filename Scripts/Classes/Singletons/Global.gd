acf5407ef443562f2490917f68e91efcad9132b2
Jeod <47716344+JeodC@users.noreply.github.com>
2025-09-18 17:00:48 -0400

    Add true portable mode

--- Scripts/Classes/Singletons/Global.gd
+++ Scripts/Classes/Singletons/Global.gd
@@ -26,11 +26,13 @@ var second_quest := false
 var extra_worlds_win := false
 const lang_codes := ["en", "fr", "es", "de", "it", "pt", "pl", "tr", "ru", "jp", "fil", "id", "ga"]
 
+var config_path : String = get_config_path()
+
 var rom_path := ""
 var rom_assets_exist := false
-const ROM_POINTER_PATH := "user://rom_pointer.smb"
-const ROM_PATH := "user://baserom.nes"
-const ROM_ASSETS_PATH := "user://resource_packs/BaseAssets"
+var	ROM_POINTER_PATH = config_path.path_join("rom_pointer.smb")
+var	ROM_PATH = config_path.path_join("baserom.nes")
+var	ROM_ASSETS_PATH = config_path.path_join("resource_packs/BaseAssets")
 const ROM_PACK_NAME := "BaseAssets"
 const ROM_ASSETS_VERSION := 0
 
@@ -169,7 +171,41 @@ func _ready() -> void:
 	get_server_version()
 	if OS.is_debug_build():
 		debug_mode = false
+	setup_configs()
 	check_for_rom()
+
+func get_config_path() -> String:
+	var exe_path := OS.get_executable_path()
+	var exe_dir  := exe_path.get_base_dir()
+	var portable_flag := exe_dir.path_join("portable.txt")
+	
+	# Test that exe dir is writeable, if not fallback to user://
+	if FileAccess.file_exists(portable_flag):
+		var test_file = exe_dir.path_join("test.txt")
+		var f = FileAccess.open(test_file, FileAccess.WRITE)
+		if f:
+			f.close()
+			var dir = DirAccess.open(exe_dir)
+			if dir:
+				dir.remove(test_file.get_file())
+			var local_dir = exe_dir.path_join("config")
+			if not DirAccess.dir_exists_absolute(local_dir):
+				DirAccess.make_dir_recursive_absolute(local_dir)
+			return local_dir
+		else:
+			push_warning("Portable flag found but exe directory is not writeable. Falling back to user://")
+	return "user://"
+
+func setup_configs() -> void:
+	var custom_chars_dir = config_path.path_join("custom_characters")
+	var custom_levels_dir = config_path.path_join("custom_levels")
+	var logs_dir = config_path.path_join("logs")
+	var resource_packs_dir = config_path.path_join("resource_packs")
+	var saves_dir = config_path.path_join("saves")
+
+	for dir in [custom_chars_dir, custom_levels_dir, logs_dir, resource_packs_dir, saves_dir]:
+		if not DirAccess.dir_exists_absolute(dir):
+			DirAccess.make_dir_recursive_absolute(dir)
 
 func check_for_rom() -> void:
 	rom_path = ""
@@ -326,7 +362,7 @@ func close_freeze() -> void:
 	$Transition/Freeze.hide()
 	$Transition.hide()
 
-var recording_dir = "user://marathon_recordings/"
+var recording_dir = config_path.path_join("marathon_recordings")
 
 func update_game_status() -> void:
 	var lives_str := str(Global.lives)
--- Scripts/Parts/ResourcePackTemplateCreator.gd
+++ Scripts/Parts/ResourcePackTemplateCreator.gd
@@ -10,17 +10,18 @@ const base_info_json := {
 	}
 
 func create_template() -> void:
+	var base_dir = Global.config_path.path_join("resource_packs/new_pack")
 	get_directories("res://Assets", files, directories)
 	for i in directories:
-		DirAccess.make_dir_recursive_absolute(i.replace("res://Assets", "user://resource_packs/new_pack"))
+		DirAccess.make_dir_recursive_absolute(i.replace("res://Assets", base_dir))
 	for i in files:
 		var destination = i
 		if destination.contains("res://"):
-			destination = i.replace("res://Assets", "user://resource_packs/new_pack")
+			destination = i.replace("res://Assets", base_dir)
 		else:
-			destination = i.replace("user://resource_packs/BaseAssets", "user://resource_packs/new_pack")
+			destination = i.replace(Global.config_path.path_join("resource_packs/BaseAssets"), base_dir)
 		print("Copying '" + i + "' to: '" + destination)
-		if i.contains(".bgm") or i.contains(".json") or i.contains("user://"):
+		if i.contains(".bgm") or i.contains(".json") or i.starts_with(base_dir):
 			DirAccess.copy_absolute(i, destination)
 		else:
 			var resource = load(i)
@@ -33,7 +34,9 @@ func create_template() -> void:
 				file.store_buffer(resource.data)
 				file.close()
 			
-	var file = FileAccess.open("user://resource_packs/new_pack/pack_info.json", FileAccess.WRITE)
+	var pack_info_path = base_dir.path_join("pack_info.json")
+	DirAccess.make_dir_recursive_absolute(pack_info_path.get_base_dir())
+	var file = FileAccess.open(pack_info_path, FileAccess.WRITE)
 	file.store_string(JSON.stringify(base_info_json, "\t"))
 	file.close()
 	print("Done")
@@ -51,7 +54,7 @@ func get_files(base_dir := "", files := []) -> void:
 			i = i.replace(".import", "")
 			print(i)
 			var target_path = base_dir + "/" + i
-			var rom_assets_path = target_path.replace("res://Assets", "user://resource_packs/BaseAssets")
+			var rom_assets_path = target_path.replace("res://Assets", Global.config_path.path_join("resource_packs/BaseAssets"))
 			if FileAccess.file_exists(rom_assets_path):
 				files.append(rom_assets_path)
 			else:
